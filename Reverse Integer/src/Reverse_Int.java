1f6ba12700fd439006ecc4115c37d9a2f1e2bc81
patflynn89 <xbl_bachhuber@hotmail.com>
2019-11-23 09:33:14 +0100

    Program *Reversed_Int.java* added

---
+++ Reverse Integer/src/Reverse_Int.java
@@ -0,0 +1,24 @@
+/**
+ * @author mat
+ * Program to reverse a given integer
+ */
+public class Reverse_Int {
+    public static void main(String[] args) {
+        int numbToReverse = 12345;
+        System.out.println("The number to reverse: " + numbToReverse);
+        int length = String.valueOf(numbToReverse).length();
+        int[] reversed = new int[length];
+        while (numbToReverse >= 10) {
+            for(int i = 0; i < length; i++) {
+                reversed[i] = numbToReverse % 10;
+                numbToReverse = numbToReverse / 10;
+            }
+        }
+
+        System.out.print("Reversed: ");
+        for(int j : reversed) {
+
+            System.out.print(j + "");
+        }
+    }
+}
